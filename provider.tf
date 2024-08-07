19722f8e7bb9b04a120c5d001835ed1b616f1a22
DevenRaut <91963968+DevenRaut@users.noreply.github.com>
2024-08-07 19:37:04 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,27 @@
+locals {
+  region = "us-east-1"
+  name   = "amonkincloud-cluster"
+  vpc_cidr = "10.123.0.0/16"
+  azs      = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
+  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
+  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
+  tags = {
+    Example = local.name
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.66.1"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = 
+  secret_key = 
+}
